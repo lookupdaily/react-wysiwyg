13ced5850bfbd14ad3c213ad5ee800bbbf5d35d1
Liz Daly <liz7daly@gmail.com>
2020-08-18 22:59:37 +0100

    Add shortcut to change to code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useMemo, useState } from 'react';
-import { createEditor } from 'slate';
+import React, { useMemo, useState, useCallback } from 'react';
+import { createEditor, Editor, Transforms } from 'slate';
 import { Slate, Editable, withReact } from 'slate-react';
 import './App.css';
+import { CodeElement, DefaultElement } from './components/Elements';
 
 const App = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -11,9 +12,33 @@ const App = () => {
       children: [{ text: 'Update this text.' }],
     },
   ]);
+
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code': 
+        return <CodeElement {...props} />
+      default: 
+        return <DefaultElement {...props} />
+    }
+  },[])
   return (
     <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
-      <Editable />
+      <Editable 
+      renderElement={renderElement}
+      onKeyDown={event => {
+        if(event.key === '`' && event.ctrlKey) {
+          event.preventDefault()
+          const [match] = Editor.nodes(editor, {
+            match: n => n.type === 'code'
+          })
+          Transforms.setNodes(
+            editor,
+            { type: match ? 'paragraph' : 'code' },
+            { match: n => Editor.isBlock(editor,n)}
+          )
+        }
+      }}
+      />
     </Slate>
   );
 };
